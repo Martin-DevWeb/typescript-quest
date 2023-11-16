4d0a7514fb8bcbfde919f1ab4924c54ae0604d13
Martin <lefils.martin@gmail.com>
2023-11-16 17:32:28 +0100

    level 1 over

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-let message: string = 'Hello World !';
-console.log(message);
+/* LEVEL 1 */
+
+// Primitive types
+let rpgName: string = "Baldur's gate III";
+let playerAge: number = 31;
+let campaignInProgress: boolean = true;
+
+// Array
+let players: string[] = ["Shadowheart", "Astarion", "Lae'zel", "Karlach"];
+
+// Type "any"
+let anyType;
+anyType = 8;
+anyType = "I can be like a string, chameleon variable !";
+
+// Function types
+function rpg(rpgName: string, players: string[]): string {
+  let playersNumber: number = players.length;
+  return rpgName.toUpperCase() + " - " + playersNumber + " joueurs.";
+}
+console.log(rpg(rpgName, players));
